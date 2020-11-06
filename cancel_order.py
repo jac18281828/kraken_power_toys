244033ccace24e2ae066301689ab4b1f17d9008f
John Cairns <john@2ad.com>
2020-11-06 12:45:22 -0600

    add cancel order

--- cancel_order.py
+++ cancel_order.py
@@ -3,21 +3,22 @@ import json,sys
 
 if __name__ == '__main__':
 
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         try:
             apikeyfile = sys.argv[1]
+            txid = sys.argv[2]
             with open(apikeyfile) as keyfile:
                 apikey = json.load(keyfile)
                 orders = KrakenOrders(apikey)
 
-                order_result = orders.query_closed()
+                print(orders.cancel_order(txid))
+
+                order_result = orders.query_closed(txid)
                 print(json.dumps(order_result, indent=4))
 
-                order_result = orders.query_open()
-                print(json.dumps(order_result, indent=4))                
         except Exception as e:
             print("Failed. "+str(e))
     else:
-        print ('apikeyfile is required')
+        print ('apikeyfile and txid are required')
         sys.exit(1)
             
--- kraken_orders.py
+++ kraken_orders.py
@@ -2,6 +2,20 @@ from kraken_request import KrakenRequest
 
 class KrakenOrders(KrakenRequest):
 
-    def query_order(self, ):
-        return self.query('/0/private/ClosedOrders', {})
+    def query_closed(self, txid):
 
+        if len(txid) > 0:
+            param = {'txid': txid}
+        else:
+            param = {}
+        
+        return self.query('/0/private/ClosedOrders', param)
+
+    def query_open(self):
+        return self.query('/0/private/OpenOrders', {})
+
+    def cancel_order(self, txid):
+        return self.query('/0/private/CancelOrder', {'txid': txid})
+
+
+    
